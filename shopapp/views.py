56bdce6fe02c4fa62f1fdb9e0b329c750ec49b96
Vipul-no-kakashi <83945410+Vipul-no-kakashi@users.noreply.github.com>
2021-10-28 13:47:14 +0530

    Trying

--- shopapp/views.py
+++ shopapp/views.py
@@ -17,11 +17,101 @@ def main_page(request, *args, **kwargs):
     }
     return render(request, 'index_mainpage.html', context=context)
 
+# def cart(request, *args, **kwargs):
+#     eid = request.session.get('eid')
+#     product_item = CartItem.objects.filter(user_id=eid)
+#     context = {
+#         "product": product_item,
+#         "id": eid
+#     }
+#     return render(request, 'cart.html', context=context)
+
+
 def cart(request, *args, **kwargs):
+    id = None
     eid = request.session.get('eid')
+    if 'name' in request.POST:
+        id = request.POST.get('name')
+
+        user_list = User.objects.filter(id=eid).values()
+        user1 = user_list[0]
+        user = user1['name']
+        product_list = Product.objects.filter(product_id=id).values()
+        product1 = product_list[0]
+        product = product1['product_name']
+        price_ht = product1['price']
+
+        final_list = CartItem.objects.filter(product_id=id, user_id=eid)
+        final_list1 = final_list
+
+        if len(final_list)==0:
+
+            list = CartItem()
+            list.product_id = id
+            list.cart_id = 1
+            list.price_ht = price_ht
+            list.user_id = eid
+            list.save()
+
+
+
+    if 'remove' in request.POST:
+        id = request.POST.get('remove')
+
+        user_list = User.objects.filter(id=eid).values()
+        user1 = user_list[0]
+        user = user1['name']
+        product_list = Product.objects.filter(product_id=id).values()
+        product1 = product_list[0]
+        product = product1['product_name']
+
+        final_list = CartItem.objects.filter(product_id=id, user_id=eid).delete()
+        # final_list1 = final_list
+
+        # if len(final_list)>0:
+
+        #     list = CartItem()
+        #     list.product_id = id
+        #     list.cart_id = 1
+        #     list.price_ht = price_ht
+        #     list.user_id = eid
+        #     list.save()
+
+
     product_item = CartItem.objects.filter(user_id=eid)
+    # cart1 = product_item[0]
+    total_price = 0
+    for x in product_item:
+        total_price += x.product.price
     context = {
         "product": product_item,
-        "id": eid
+        "id": id,
+        "eid":eid,
+        "total": total_price
+        # "cart": cart1
     }
     return render(request, 'cart.html', context=context)
+
+
+
+
+def main_page_product(request, id=None, *args, **kwargs):
+    # article_obj = None
+    if id is not None:
+        product_item = Product.objects.filter(product_id=id)
+        context = {
+            "product": product_item
+            # "userid": userid
+        }
+        return render(request, 'index_itempage.html', context=context)
+
+
+def main_page_cart_product(request, id=None, *args, **kwargs):
+    # article_obj = None
+    if id is not None:
+        product_item = Product.objects.filter(product_id=id)
+        context = {
+            "product": product_item
+            # "userid": userid
+        }
+        return render(request, 'index_itempage1.html', context=context)
