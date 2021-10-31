a1d6936504c0f4fa34f3d572cd7cb886f8949cff
Mayank <tayalmayank2000@gmail.com>
2021-10-31 19:14:38 +0530

    commit1

--- shopapp/admin.py
+++ shopapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Cart, CartItem
+from .models import Product, Cart, CartItem, Orders
 
 
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(Orders)
